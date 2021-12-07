2ae20f5923db85b444b0a1dc4964359fcff6f330
Geraldo Boueres <geraldoboueres@gmail.com>
2021-12-06 20:34:44 -0400

    Header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,30 @@
+import {Flex, Button, Stack} from '@chakra-ui/react'
+import { Input } from '../components/Form/Input'
+
 export default function Home() {
   return (
-   <h1>Hello World</h1>
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+    
+    <Flex
+    as="form"
+    width="100%"
+    maxWidth={360}
+    bg="gray.700"
+    p="4"
+    borderRadius={8}
+    flexDirection="column"
+    >
+      <Stack spacing="4">
+        <Input type="email" name="email" label="E-mail" />
+        <Input type="password" name="password" label="Password" />
+      
+      </Stack>
+      <Button type="submit" mt="6" colorScheme="pink"> Larissa Linda </Button>
+
+      
+    </Flex>
+
+    </Flex>
+   
   )
 }
